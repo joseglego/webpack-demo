08b7a3602b6afb6b5a517fd68ab908906b26e48e
José Lezama <joseglego@gmail.com>
2020-03-30 19:09:31 -0300

    feat(): configure data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.svg';
+import XMLData from './data.xml';
+import JSONData from './data.json';
 
 function component() {
   const element = document.createElement('div');
@@ -15,6 +17,10 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+
+  // Add DATA
+  console.log('XML Data: ', XMLData);
+  console.log('JSON Data: ', JSONData);
 
   return element;
 }
