6c6b88206e6f3ba5198712719f7e38d72e51e26b
José Lezama <joseglego@gmail.com>
2020-03-30 17:15:27 -0300

    feat(): configure css

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## Steps
 - Step 0: [Getting Started](https://webpack.js.org/guides/getting-started/)
+- Step 1: [Asset Management](https://webpack.js.org/guides/asset-management/)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  // Add CSS
+  element.classList.add('hello');
 
   return element;
 }
